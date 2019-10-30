6a1a358776863860a43ea790325c1eb31fd233c1
lijiajia88 <apple@appledeMacBook-Pro.local>
2019-10-30 19:18:01 +0800

    lab1

--- lab1/bc.c
+++ lab1/bc.c
@@ -1,27 +1,43 @@
-#include <stdio.h>
-#include <stdlib.h>
+#include<stdio.h>
 
-int main(void){
-    int n;
-    int i=0;
-    for(i=0;i<7;++i);
-    int a[100];
-    int count=0;
-    printf("请输入一个十进制正整数：\n");
-    scanf("%d",&n);
-    printf("对应的二进制整数是：")；
-    while (n){
-        a[i]=n%2;
-        i++;
-        n/=2;
-    }
-    for(int j=i-1;j>=0;j--){
-        printf("%d",a[j]);
-        if(a[j]==1){
-            count++;
-        }
-    }
-    printf("\n\"1\"出现的次数是%d\n",count);
-    system("pause");
-    return 0;
+int main()
+{
+	char Input_char;
+	int dec[7];
+	int i = 0,j = 0,k,Input,Count = 0;
+	int bin[20];
+	
+	
+	while((Input_char = getchar()) != '\n')						
+	{
+		dec[i] = (int)(Input_char-'0');							
+		i++;
+	}
+	i--;														
+	for(;i >= 0;i--)											
+	{
+		for(k = 0;k < j;k++)
+		{
+			dec[i] = dec[i]*10;
+		}
+		Input = Input+dec[i];
+		j++;
+	}
+	
+	
+	for(i=0;Input > 0;i++)											
+	{
+		bin[i] = ((Input & 1) == 0 ? 0 : 1);					
+		Input >>= 1;											
+	}
+	i--;
+	for(;i >= 0;i--)
+	{
+		printf("%d",bin[i]);
+		if(bin[i] == 1)											
+			Count++;									
+	}
+	printf("\n1�ĸ���Ϊ%d",Count);							
+	
+	
 }
